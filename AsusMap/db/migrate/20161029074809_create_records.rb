e5b4bd5a1804fa53fcb25ee999b2c3ddfd62a742
Kiwi <kiwi010379@gmail.com>
2016-10-29 15:55:37 +0800

    Initialize Rails project

---
+++ AsusMap/db/migrate/20161029074809_create_records.rb
@@ -0,0 +1,12 @@
+class CreateRecords < ActiveRecord::Migration[5.0]
+  def change
+    create_table :records do |t|
+      t.float, :latitude
+      t.float, :longitude
+      t.int, :device_id
+      t.datetime :created_at
+
+      t.timestamps
+    end
+  end
+end
