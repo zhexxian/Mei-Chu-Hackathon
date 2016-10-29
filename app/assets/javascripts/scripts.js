765d57f4912a1a5d01706c6e2abce700eab40702
Kiwi <kiwi010379@gmail.com>
2016-10-29 17:44:57 +0800

    Merge front-end files

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    // $("button").click(function(){
+	   //  $.ajax({
+    //         type: 'GET',
+    //         url: 'http://www.google.com',
+    //         //data: "Hi",
+    //         //dataType: 'text',
+    //         success: function(result) {
+    //             alert("Info sent to backend!");
+    //         },
+    //         error: function(result) {
+    //             alert("Info NOT sent to backend!");
+    //         }        
+    //     });
+    // });
+   
+});
+
+//function myFunction() {
+//}
+
+//document.getElementById("button").onclick = function() {myFunction()};
